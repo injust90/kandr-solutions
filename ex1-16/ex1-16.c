08d0deed5dc73ce8a8f4c02dd36e61381dfa0057
Justin Chou <justinjchou@gmail.com>
2025-11-08 15:46:56 -0800

    some updates

--- ex1-16/ex1-16.c
+++ ex1-16/ex1-16.c
@@ -8,36 +8,36 @@ void copy(char to[], char from[]);
 int main()
 {
 	int currentLength;	// current line length
-	int max;	// maximum length seen so far
+	int maxlengthseen;	// maximum length seen so far
 	char line [MAXLINE]; 	// curent input line
 	char longest[MAXLINE]; 	// longest line saved here
 
-	max = 0;
+	maxlengthseen = 0;
 	// while there's another line, if (it's longer than the previous longest) save it
 	// save its length
 	// print longest line
 	while ((currentLength = getline1(line, MAXLINE)) > 0)
-		if (currentLength > max) {
-			max = currentLength;
+		if (currentLength > maxlengthseen) {
+			maxlengthseen = currentLength;
 			copy(longest, line);
 		}
-	if (max > 0)	// there was a line
+	if (maxlengthseen > 0)	// there was a line
 		printf("%s", longest);
 	return 0;
 }
 
 // getline: read a line int s, return length
-int getline1(char s[], int limit)
+int getline1(char newLine[], int limit)
 {
 	int c, length;
 
 	for (length = 0; length < limit - 1 && (c = getchar())!=EOF && c!='\n'; ++length)
-		s[length] = c;
+		newLine[length] = c;
 	if (c == '\n') {
-		s[length] = c;
+		newLine[length] = c;
 		++length;
 	}
-	s[length] = '\0';
+	newLine[length] = '\0';
 	printf("Length: %d\n", length);
 	return length;
 }
