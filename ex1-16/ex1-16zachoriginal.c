b81ca435976d92c070f978aba4bf46a808c2f758
Justin Chou <justinjchou@gmail.com>
2025-11-10 10:15:55 -0800

    added some comments

--- ex1-16/ex1-16zachoriginal.c
+++ ex1-16/ex1-16zachoriginal.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXLINE 3 
+#define MAXLINE 1000
 
-int getline(char line[], int maxline);
 int main() {
     char line[MAXLINE];
     char longest[MAXLINE];
@@ -10,23 +9,22 @@ int main() {
 
     printf("Enter text (Ctrl+D to end):\n");
 
-// while you are getting in the sentence, you are setting length to the strlen of the sentence
-	// using fgets to read in the sentence
-
-    while ((line, sizeof(line), stdin)) {
+    // fgets reads characters from the character stream and stores them in the character array
+    // stores the line first by limit of the size of the string stdin is the stream
+    // then you set length to the string length
+    while (fgets(line, sizeof(line), stdin)) {
         len = strlen(line);
 
-	// while length is greater than zero and character is not equal to \n, and not EOF
+	// while length is greater than zero AND line at point is \n, AND foef stdin
         while (len > 0 && line[len - 1] != '\n' && !feof(stdin)) {
-		// set array equal to the pre-established limit
-		char cont[MAXLINE];
-		// if fgets isnot available, break
+            char cont[MAXLINE];
             if (!fgets(cont, sizeof(cont), stdin)) break;
-		// length += length
             len += strlen(cont);
         }
 
-	// compare to establish longest length
+	// if length greater than max length
+	// max length is length
+	// strcpy longest to line
         if (len > max_len) {
             max_len = len;
             strcpy(longest, line);
@@ -37,21 +35,4 @@ int main() {
         printf("\nMax length: %d\nText: %s\n", max_len, longest);
 
     return 0;
-}
-
-int getline1(char s[], int lim)
-{
-	int c, i;
-
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c!='\n'; ++i)
-		s[i] = c;
-	
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-
-	s[i] = '\0';
-
-	return i;
 }
