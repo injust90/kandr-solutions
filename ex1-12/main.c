b6acc3ec35bf610ea6f93724ec1b3d7148aa7de7
Justin Chou <justinjchou@gmail.com>
2025-11-12 12:51:55 -0800

    F grade ex1-12

---
+++ ex1-12/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define IN 1 // inside a word
+#define OUT 0 // outside a word
+
+int main()
+{
+	int c, n1, nw, nc, state;
+
+	state = OUT;
+	n1 = nw = nc = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == 't') {
+			state = OUT;
+			putchar('\n');
+		}
+		else {
+			state = IN;
+		}
+		if (state == IN) {
+			putchar(c);
+			state = OUT;
+		}
+	}
+}
