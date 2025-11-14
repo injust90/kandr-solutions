1db82b675179f3929b4aab152a6fc768db800c31
Justin Chou <justinjchou@gmail.com>
2025-11-13 18:09:44 -0800

    ex1-14 finished!

--- ex1-14/main.c
+++ ex1-14/main.c
@@ -16,16 +16,20 @@ int main() {
 	int character = 'a';
 
 	for (int i = 0; i < 255; ++i) {
-		for (int j = 0; j < counter[i]; ++j) {
-			putchar('*');
-		}
 		if (counter[i] > 0 && i != 10) {
-			printf("%c Counter: %d\n", i, counter[i]);
+			printf("%c Counter: %d", i, counter[i]);
+			for (int j = 0; j < counter[i]; ++j) {
+				putchar('*');
+			}
+			putchar('\n');
 		}
 		else if (i == 10) {
-			printf("\\n Counter: %d\n", counter[i]);
+			printf("\\n Counter: %d", counter[i]);
+			for (int j = 0; j < counter[i]; ++j) {
+				putchar('*');
+			}
+			putchar('\n');
 		}
 	}
-
 	return 0;
 }
